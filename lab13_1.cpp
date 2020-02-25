cb0cc1674944f30f51ceca24f9efe8edb273ef56
PlaneCargo191 <t.james_17@hotmail.com>
2020-02-25 14:43:01 +0700

    New

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,17 +3,56 @@
 using namespace std;
 
 template <typename T>
-void swap(T d[],int x,int y){
+void swap(T d[],int x,int y)
+{
     T temp = d[x];
     d[x] = d[y];
     d[y] = temp;
 }
 
 template <typename T>
-void insertionSort(T d[],int N){
-	
-//Write your code here.
+void insertionSort(T d[],int N)
+{
+	for(int j=1; j<N; j++)
+	{
+		int z;
+		for(int k=0; k<N; k++)
+		{
+			if(k==j)
+			{
+				cout << "[" << d[j] << "] ";
+				z=d[j];
+			}
+			else
+			{
+				cout << d[k] << " ";
+			}
+		}
 
+		cout << "=> ";
+
+		for(int i=j; i>=0; i--)
+		{
+			if(d[i]>d[i-1])
+			{
+				swap(d, i, i-1);
+			}
+		}
+
+		for(int h=0; h<N; h++)
+		{
+			if(d[h]==z)
+			{
+				cout << "[" << z << "] ";
+				z++;
+			}
+			else
+			{
+				cout << d[h] << " ";
+			}
+		}
+		cout << "\n";
+	}
 }
 
 int main(){
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -20,4 +20,50 @@ int main(){
 	return 0;
 }
 
-// Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+void inputMatrix(double a[][N])
+{
+	for(int i=0; i<N; i++)
+	{
+		int j=0;
+		cout << "Row " << i+1 <<": ";
+		cin >> a[i][0] >> a[i][1] >> a[i][2] >> a[i][3] >> a[i][4];
+	}
+}
+
+void findLocalMax(const double b[N][N], bool c[N][N])
+{
+	for(int j=0; j<N; j++)
+	{
+		for(int k=0; k<N; k++)
+		{
+			if(b[j][k] >= b[j-1][k] && b[j][k] >= b[j+1][k] && b[j][k] >= b[j][k-1] && b[j][k] >= b[j][k+1])
+			{
+				c[j][k]=1;
+			}
+			else
+			{
+				c[j][k]=0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool d[N][N])
+{
+	for(int j=0; j<N; j++)
+	{
+		for(int k=0; k<N; k++)
+		{
+			if(j==0 || j==4 || k==0 || k==4)
+			{
+				cout << "0" << " ";
+			}
+			else
+			{
+				if(d[j][k]==1) cout << "1" << " ";
+				else cout << "0" << " ";
+			}
+		}
+		cout << endl;
+	}
+}
